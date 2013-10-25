d83e4f5aa2a78b6158c42a925007f90e35dcf29a
Norman Hilbert <mail@nhilbert.de>
2013-10-25 20:36:27 +0200

    Started writing JUnit Tests

---
+++ test/de/nhilbert/zenbell/BellTest.java
@@ -0,0 +1,14 @@
+package de.nhilbert.zenbell;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BellTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
