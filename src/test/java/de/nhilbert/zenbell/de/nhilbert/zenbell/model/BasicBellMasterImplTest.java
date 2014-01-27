64fd108746b66ae7519a104be60c636f6db9623a
Norman Hilbert <norman.hilbert@allolio-konrad.com>
2014-01-27 22:02:40 +0100

    Framework for testing the BellMasterImplementation

---
+++ src/test/java/de/nhilbert/zenbell/de/nhilbert/zenbell/model/BasicBellMasterImplTest.java
@@ -0,0 +1,9 @@
+package de.nhilbert.zenbell.de.nhilbert.zenbell.model;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by norman.hilbert on 27.01.14.
+ */
+public class BasicBellMasterImplTest extends TestCase {
+}
