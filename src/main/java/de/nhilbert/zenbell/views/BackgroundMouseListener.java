72adb237a50e19081c28148b826f773b8f4f2062
Norman Hilbert <norman.hilbert@allolio-konrad.com>
2014-02-01 12:34:06 +0100

    Commenting the Background MouseListener

--- src/main/java/de/nhilbert/zenbell/views/BackgroundMouseListener.java
+++ src/main/java/de/nhilbert/zenbell/views/BackgroundMouseListener.java
@@ -8,16 +8,26 @@ import java.awt.event.MouseListener;
 /**
  * Created by norman.hilbert on 27.01.14.
  * Listens to mouse activities on the background image
+ *
+ * The Background Mouse Listener activates action such as showing a context menu and ringing the bell
+ * on a click on the background.
  */
 public class BackgroundMouseListener implements MouseListener {
 
     private MainPresenter myPresenter;
 
+    /**
+     * This the constructor for the BackgroundMouseListener
+     * @param myPresenter the presenter for the JFRAME holding the image
+     */
     public BackgroundMouseListener(MainPresenter myPresenter){
         this.myPresenter=myPresenter;
     }
 
-
+    /***
+     * When the mouse is clicked, if it is the left button, ring the bell
+     * @param e
+     */
     @Override
     public void mouseClicked(MouseEvent e) {
         if(e.getButton()==MouseEvent.BUTTON1){
@@ -26,6 +36,10 @@ public class BackgroundMouseListener implements MouseListener {
         }
     }
 
+    /**
+     * Whenever the right button is pressed, show a context menu, allow to edit settings etc.
+     * @param e
+     */
     @Override
     public void mousePressed(MouseEvent e) {
         //on right click show popup
@@ -37,16 +51,28 @@ public class BackgroundMouseListener implements MouseListener {
         }
     }
 
+    /**
+     * No action
+     * @param e
+     */
     @Override
     public void mouseReleased(MouseEvent e) {
 
     }
 
+    /**
+     * No action
+     * @param e
+     */
     @Override
     public void mouseEntered(MouseEvent e) {
 
     }
 
+    /**
+     * No action
+     * @param e
+     */
     @Override
     public void mouseExited(MouseEvent e) {
 
